aad2788a6055c90bf337a0f7b70dd0cb2c3a2616
ferrandf <ferrandelga24@gmail.com>
2023-11-30 17:49:32 +0100

    Adding acceptor drop

--- acceptor.erl
+++ acceptor.erl
@@ -1,66 +1,87 @@
 -module(acceptor).
 -export([start/2]).
--define(delay_promise, 8000).
--define(delay_vote, 8000).
--define(drop, 1).
+-define(delay_promise, 3000).
+-define(delay_accept, 3000).
+-define(drop,1).
+
 
 start(Name, PanelId) ->
-    spawn(fun() -> init(Name, PanelId) end).
-
+  spawn(fun() -> init(Name, PanelId) end).
+        
 init(Name, PanelId) ->
-    Promised = order:null(),
-    Voted = order:null(),
-    Value = na,
-    acceptor(Name, Promised, Voted, Value, PanelId).
+  Promised = order:null(), 
+  Voted = order:null(),
+  Value = na,
+  acceptor(Name, Promised, Voted, Value, PanelId).
 
-getdelay() ->
-   list_to_integer(os:getenv("delay_shell")).
+%getdelay() ->
+%  list_to_integer(os:getenv("delay_shell")).
 
-send_or_drop(Proposer, message) ->
+getdrop() ->
   P = rand:uniform(10),
-  if P =< ?drop ->
-        io:format("message dropped~n");
-     true -> Proposer ! message, true
+  P =< list_to_integer(os:getenv("drop")).
+
+send_or_drop(Proposer, Message) ->
+  case getdrop() of
+    true -> io:format("dropping message~n",[]);
+    false -> Proposer ! Message
   end.
+
 
 acceptor(Name, Promised, Voted, Value, PanelId) ->
   receive
     {prepare, Proposer, Round} ->
       case order:gr(Round, Promised) of
         true ->
-            T = rand:uniform(getdelay()),
-            timer:send_after(T, Proposer, {promise, Round, Voted, Value}),
-          % Proposer ! {promise, Round, Voted, Value},               
-      io:format("[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w delayed ~w~n",
-                 [Name, Round, Voted, Value, T]),
+
+          %Original -----------------------------
+          %Proposer ! {promise, Round, Voted, Value}, %- original
+
+          %Experiment i.1)-----------------------------
+          %T = rand:uniform(getdelay()),
+          %timer:send_after(T,Proposer,{promise, Round, Voted, Value}),
+
+          %Experiment iii)-----------------------------
+          send_or_drop(Proposer,{promise, Round, Voted, Value}),
+
+      io:format("[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w~n",
+        [Name, Round, Voted, Value]),
           % Update gui
           Colour = case Value of na -> {0,0,0}; _ -> Value end,
           PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Voted]), 
-                     "Promised: " ++ io_lib:format("~p", [Round]), Colour},
-          acceptor(Name, Round, Voted, Value, PanelId);
+                     "Promised: " ++ io_lib:format("~p", [Round]), Colour}, % Round
+          acceptor(Name, Round, Voted, Value, PanelId); % Round
         false ->
           Proposer ! {sorry, {prepare, Round}},
           acceptor(Name, Promised, Voted, Value, PanelId)
       end;
     {accept, Proposer, Round, Proposal} ->
-      case order:goe(Round, Promised) of
+      case order:goe(Round, Promised) of %Promised
         true ->
-          T = rand:uniform(getdelay()),
-          timer:send_after(T, Proposer, {vote, Round}),
-          % Proposer ! {vote, Round},
+
+          %Original -----------------------------
+          %Proposer ! {vote, Round}, % Proposer ! {vote, Round}
+
+          %Experiment i.1) -----------------------------
+          %T = rand:uniform(getdelay()),
+          %timer:send_after(T,Proposer,{vote, Round}),
+
+          % Experiment iii) -----------------------------
+          send_or_drop(Proposer,{vote, Round}),
+          
           case order:goe(Round, Voted) of
             true ->
-      io:format("[Acceptor ~w] Phase 2: promised ~w voted ~w colour ~w delayed ~w~n",
-                 [Name, Promised, Round, Proposal, T]),
+      io:format("[Acceptor ~w] Phase 2: promised ~w voted ~w colour ~w~n",
+                 [Name, Promised, Round, Proposal]), % Name, Promised, Round, Proposal
               % Update gui
-              PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Round]), 
-                         "Promised: " ++ io_lib:format("~p", [Promised]), Proposal},
+              PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Round]), % Round
+                         "Promised: " ++ io_lib:format("~p", [Promised]), Proposal}, %Proposal
               acceptor(Name, Promised, Round, Proposal, PanelId);
             false ->
-              acceptor(Name, Promised, Voted, Value, PanelId)
-          end;                            
+              acceptor(Name, Promised, Voted, Value, PanelId) %Round vs Voted
+          end;
         false ->
-          Proposer ! {sorry, {accept, Round}},
+          Proposer ! {sorry, {accept, Round}}, %maybe optimization: Promised here
           acceptor(Name, Promised, Voted, Value, PanelId)
       end;
     stop ->
